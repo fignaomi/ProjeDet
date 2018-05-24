6a95fa81edc4c31fe2154535df4dba5c5c471375
gutluana <36938558+gutluana@users.noreply.github.com>
2018-05-23 21:44:59 -0300

    Add files via upload

---
+++ ProjeDet/CodigoFonte/bd_inserepergunta2.php
@@ -0,0 +1,23 @@
+﻿<?php
+	include "template/topo.php";
+	
+		  // RECEBE VARIAVEIS
+		  $nome=$_POST['login'];
+		  $senha=$_POST['senha'];
+
+		  
+		   if (!$nome || !$senha)
+		  {
+			 echo 'voce não preencheu todos os dados<br />';
+			 exit;
+		  }
+		  
+		  //inserindo dados do formulario no bd
+		  $query = "insert into aluno values (null,'$nome','$senha')";
+		  $result = mysqli_query($db,$query);
+		  if ($result)
+			   echo  mysqli_affected_rows($db).' Cadastro Feito com sucesso!</br>'; 
+		  else echo mysqli_error($db).'<br>';
+		  
+	include "template/rodape.php";
+?>
