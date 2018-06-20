14c229e9a22130132b490805718da6609bda027f
gutluana <36938558+gutluana@users.noreply.github.com>
2018-06-20 20:00:12 -0300

    Add files via upload

--- ProjeDet/CodigoFonte/bd_inserepergunta.php
+++ ProjeDet/CodigoFonte/bd_inserepergunta.php
@@ -2,30 +2,25 @@
 	include "template/topo.php";
 	
 		  // RECEBE VARIAVEIS
-		  $codpg=$_POST['cod_pergunta'];
-		  $descricao=$_POST['descricao'];
-		  $resposta_certa=$_POST['resposta_certa'];
-		  $resposta_errada1=$_POST['resposta_errada1'];
-		  $resposta_errada2=$_POST['resposta_errada2'];
-		  $resposta_errada3=$_POST['resposta_errada3'];
-		  $resposta_errada4=$_POST['resposta_errada4'];
-		  $codpf=$_POST['cod_professor'];
-		  $coda=$_POST['cod_atividade'];
-
-				  
-
-		   if (!$codpg || !$descricao || !$resposta_certa || !$resposta_errada1 || !$resposta_errada2 || !$resposta_errada3 || !$resposta_errada4 || !$codpf || !$coda)
+		  $nome=$_POST['login'];
+		  $senha=$_POST['senha'];
+		  
+		   if (!$nome || !$senha)
 		  {
 			 echo 'voce não preencheu todos os dados<br />';
 			 exit;
 		  }
 		  
 		  //inserindo dados do formulario no bd
-		  $query = "insert into pergunta values (null,'$descricao','$resposta_certa','$resposta_errada1','$resposta_errada2','$resposta_errada3','$resposta_errada4','$codpf','$coda')";
+		  $query = "insert into professor values (null,'$nome','$senha')";
 		  $result = mysqli_query($db,$query);
-		  if ($result)
-			   echo  mysqli_affected_rows($db).' pergunta inserida no BD.</br>'; 
-		  else echo mysqli_error($db).'<br>';
+		  if ($result){
+			 
+			header("location:form_pesquisa1.php?add=1");
+		  }else echo mysqli_error($db).'<br>';
+			  // echo  mysqli_affected_rows($db).' Cadastro feito com sucesso!</br>';
+			
+			
 		  
-	include "template/rodape.php";
 ?>
+	
