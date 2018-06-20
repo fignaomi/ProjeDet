73449d1c541febe70eb8f92ae58e319db4dbf15c
gutluana <36938558+gutluana@users.noreply.github.com>
2018-06-20 19:59:15 -0300

    Add files via upload

---
+++ ProjeDet/CodigoFonte/bd_insere.php
@@ -0,0 +1,21 @@
+﻿<?php
+	include "template/topo.php";
+	
+		  // RECEBE VARIAVEIS
+		  $nome=$_POST['login'];
+		  $senha=$_POST['senha'];
+		  
+		   if (!$nome || !$senha)
+		  {
+			 echo 'voce não preencheu todos os dados<br />';
+			 exit;
+		  }
+		  
+		  //inserindo dados do formulario no bd
+		  $query = "insert into professor values (null,'$nome','$senha')";
+		  $result = mysqli_query($db,$query);
+		  if ($result)
+			   echo  mysqli_affected_rows($db).' Entrou!</br>'; 
+		  else echo mysqli_error($db).'<br>';
+?>
+	
